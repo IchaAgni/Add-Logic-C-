abb5d97a7057a440576dc268f70ae39ba832fef1
IchaAgni <98201578+IchaAgni@users.noreply.github.com>
2022-10-18 13:46:29 +0700

    Add files via upload

---
+++ DoWhileStatement/DoWhileStatement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int current = 0;
+
+            do
+            {
+                current = random.Next(1, 11);
+                Console.WriteLine(current);
+            } while (current != 7);
+
+        }
+    }
+}
